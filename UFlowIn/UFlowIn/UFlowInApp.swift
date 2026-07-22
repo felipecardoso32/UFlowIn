cad12695d49e694daeb101e20aaa3e85cb0716a7
Felipe Colares Cardoso <151647095+felipecardoso32@users.noreply.github.com>
2026-07-22 05:33:44 -0300

    Projeto
    Projeto adicionado

---
+++ UFlowIn/UFlowIn/UFlowInApp.swift
@@ -0,0 +1,17 @@
+//
+//  UFlowInApp.swift
+//  UFlowIn
+//
+//  Created by Felipe Colares Cardoso on 22/07/26.
+//
+
+import SwiftUI
+
+@main
+struct UFlowInApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
